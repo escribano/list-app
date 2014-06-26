5ef6df9aae8e8cd1084c55c47bd20e4d1033c790
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-06-26 01:57:59 -0700

    Some experimentation with gorilla, mostly routing/mux biz.

--- main.go
+++ main.go
@@ -4,13 +4,31 @@ import (
 	"database/sql"
 	_ "github.com/lib/pq"
 
+	"github.com/gorilla/mux"
+
 	"fmt"
+	"net/http"
 )
 
+func index(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "<h1>HI</h1>")
+}
+
 func main() {
-	db, err := sql.Open("postgres", "user=list_app dbname=list_app  sslmode=verifyfull")
+	_, err := sql.Open("postgres", "user=list_app dbname=list_app  sslmode=verifyfull")
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	router := mux.NewRouter()
+
+	// Serve static directory
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	router.HandleFunc("/", index)
+	router.HandleFunc("/{name}", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(res, "<h1>Hello there, Mc-%s</h1>", req.URL.Path[1:])
+	})
+
+	http.Handle("/", router)
+	http.ListenAndServe(":8088", nil)
 }
