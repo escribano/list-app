ce887c025d5ad5eaff1c5ae92259132a31f57421
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-06-26 23:31:54 -0700

    Added in comment for schema lib usage.

--- handlers.go
+++ handlers.go
@@ -7,6 +7,20 @@ import (
 )
 
 func IndexHandler(res http.ResponseWriter, req *http.Request) {
+	// login := new(LoginForm)
+
+	// err := req.ParseForm()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// err = decoder.Decode(login, req.PostForm)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// //call AuthPass
+	// fmt.Println(login)
+
 	body, _ := ioutil.ReadFile("templates/index.html")
 	fmt.Fprint(res, string(body))
 }
