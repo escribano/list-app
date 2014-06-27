192440af3478c8de5f6610d2a1b741e94084bd0e
Eric Clark <ericbrendanclark@gmail.com>
2014-06-26 22:44:50 -0700

    added minimal password creation and authentication functions.

--- auth.go
+++ auth.go
@@ -7,14 +7,16 @@ import (
 	//"github.com/gorilla/rpc/json"
 	"github.com/gorilla/schema"
 	//"github.com/gorilla/sessions"
-
+	"crypto/sha512"
+	"crypto/rand"
+	"code.google.com/p/go.crypto/pbkdf2"
 	"fmt"
 	"net/http"
 )
 
 type CreateAccountForm struct {
 	FirstName string
-	LastName  String
+	LastName  string
 	Username  string
 	Email     string
 	Password  string
@@ -40,11 +42,11 @@ func LoginHandler(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	//call AuthPass
 	fmt.Println(login)
 }
 
-func NewAccountHandler(res http.RequestWriter, req *http.Request) {
+func NewAccountHandler(res http.ResponseWriter, req *http.Request) {
 	newUser := new(CreateAccountForm)
 
 	err := req.ParseForm()
@@ -56,6 +58,31 @@ func NewAccountHandler(res http.RequestWriter, req *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
+	//call NewPass
 
 	fmt.Println(newUser)
+}
+
+func NewPass(password string) ([]byte, []byte) {
+	// make user salt and hash of pass and put into database
+	// for salt may want to have seperate database that stores all salts ever made
+	// to make sure none are reused.
+	salt := []byte("fixed_test_salts")
+	fmt.Println(len(salt))
+	//salt := make([]byte, 16)
+	//_, err := rand.Read(salt)
+	//if err != nil {
+	//fmt.Println(err)
+	//}
+	return pbkdf2.Key([]byte(password), salt, 4096, 64, sha512.New), salt
+}
+
+
+func AuthPass(password, user string) bool {
+	// pull user's salt and password from database
+	userSalt := []byte("fixed_test_salts")
+	//hash potential input password
+	passHash := pbkdf2.Key([]byte(login.Password), userSalt, 4096, 64, sha512.New)
+	// compare passHash to the hash in the database, if match login is correct(true), if not login failed(false).
+	return true
 }
