4dc7ab89847ba88492bc3051df9c8196176c1912
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-06-26 22:56:59 -0700

    Added two new handlers for template as well as renamed account.html

--- handlers.go
+++ handlers.go
@@ -10,3 +10,13 @@ func IndexHandler(res http.ResponseWriter, req *http.Request) {
 	body, _ := ioutil.ReadFile("templates/index.html")
 	fmt.Fprint(res, string(body))
 }
+
+func NewAccountHandler(res http.ResponseWriter, req *http.Request) {
+	body, _ := ioutil.ReadFile("templates/new-account.html")
+	fmt.Fprint(res, string(body))
+}
+
+func LoginHandler(res http.ResponseWriter, req *http.Request) {
+	body, _ := ioutil.ReadFile("templates/login.html")
+	fmt.Fprint(res, string(body))
+}
