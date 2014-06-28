309afccba066c3f5687a97b47039a216a86550a9
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-06-28 00:34:51 -0700

    Simple for validation methodology. More testing with sessions.
    I have designed a simple form validation method that may or may not stay the same
    with the usage of json. You have the form struct and a Validate interface. This makes
    form validtion rather easy.
    
    I am messing with sessions more to understand them fully. Next steps are to generate
    random strings for the name of a new session (like when logging in) and store that
    random string while also associating it with a user. From there you can set the values
    about authenticated status and other sensitive things in the database reducing
    security risks. This may mean we can just use the secure cookie package.

--- handlers.go
+++ handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	//"github.com/gorilla/rpc"
+	//"github.com/gorilla/rpc/json"
 	"github.com/gorilla/schema"
 
 	"fmt"
@@ -10,57 +12,91 @@ import (
 
 var decoder = schema.NewDecoder()
 
+// Get index.html form
 func Index(res http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.Cookie("session-name"))
 	body, _ := ioutil.ReadFile("templates/index.html")
 	fmt.Fprint(res, string(body))
 }
 
+// Get register.html form
 func GetNewAccount(res http.ResponseWriter, req *http.Request) {
-	body, _ := ioutil.ReadFile("templates/new-account.html")
+	body, _ := ioutil.ReadFile("templates/register.html")
 	fmt.Fprint(res, string(body))
 }
 
+// Handler for making a new user
 func PostNewAccount(res http.ResponseWriter, req *http.Request) {
 	newUser := new(CreateAccountForm)
 
 	if err := req.ParseForm(); err != nil {
 		fmt.Println("ERROR parsing form: ", err)
 	}
-
 	if err := decoder.Decode(newUser, req.PostForm); err != nil {
-		// Could return an error here becuase it implies an emtpy field
 		fmt.Println("ERROR decoding form: ", err)
+	}
+
+	if err := newUser.Validate(); err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(res, err.Error())
+		return
 	}
 
 	var hash, salt string
 	if newUser.Password == newUser.Password2 {
 		hash, salt = NewPass(newUser.Password)
-	} else {
-		// PASSWORD DONT MATCH YO
-		// Set a flash message and return form data except passwords?
 	}
 
-	queryString := fmt.Sprintf(`INSERT INTO users (first_name, last_name, email, hash, salt) VALUES ("%s", "%s", "%s", E"%s", E"%s");`,
-		newUser.FirstName, newUser.LastName, newUser.Email, hash, salt)
-	fmt.Println(queryString)
-	results, err := DB.Exec(queryString)
-	if err != nil {
-		fmt.Println("ERROR inserting new user: ", err)
-	}
-	fmt.Println(results)
+	// API call here? api.InsertNewUser()
 
+	fmt.Println(newUser, hash, salt)
 	fmt.Fprint(res, newUser)
 }
 
+// Get login.html form
 func GetLogin(res http.ResponseWriter, req *http.Request) {
 	body, _ := ioutil.ReadFile("templates/login.html")
 	fmt.Fprint(res, string(body))
 }
 
+// Handler for authenticating a user
 func PostLogin(res http.ResponseWriter, req *http.Request) {
+	user := new(LoginForm)
+	if err := req.ParseForm(); err != nil {
+		fmt.Println("ERROR parsing form: ", err)
+	}
+	if err := decoder.Decode(user, req.PostForm); err != nil {
+		fmt.Println("ERROR decoding form: ", err)
+	}
 
-}
+	if err := user.Validate(); err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(res, err.Error())
+		return //and redirect or something
+	}
 
-func GetAllTasks(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("AUTHED USER: ", user)
+	// get user salt and hash to validate
+	// if ok, redirect, otherwise rety login
+
+	// Make a new session with a random string as the name.
+	// Save that string in the store or something so that none
+	// of that data is client side.
+	session, err := Sessions.Get(req, "session-name")
+	fmt.Println(session.Name())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	session.Values["authenticated"] = true
+	//session.Values["id"] = API CALL
+	if user.RememberMe {
+		// Set session for 14 days
+		//session.Options.MaxAge = 86400 * 14
+		session.Options.MaxAge = 60
+	} else {
+		session.Options.MaxAge = 30
+	}
+	session.Save(req, res)
 
 }
