5e09dbe42bfb042f38478d4c5e6e60b15815638d
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-07-03 21:13:27 -0700

    Changed dev route to show new list view testing.

--- handlers/statics.go
+++ handlers/statics.go
@@ -32,6 +32,6 @@ func GetListView(res http.ResponseWriter, req *http.Request) {
 
 // list-view.html page
 func DevList(res http.ResponseWriter, req *http.Request) {
-	body, _ := ioutil.ReadFile("templates/list-view-lite.html")
+	body, _ := ioutil.ReadFile("templates/list-view-v2.html")
 	fmt.Fprint(res, string(body))
 }
