84864759cdfd8e88410addda61cb612e78483aa8
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-07-06 14:24:49 -0700

    Javascript messing, also messing with masonry. TODO: Galen do his side of masonry setup.

--- static/js/app.js
+++ static/js/app.js
@@ -1,54 +1,37 @@
-var taskApp = angular.module("taskApp", []);
+var ListApp = angular.module("ListApp", []);
 
-taskApp.service('Tasks', ['$rootScope', '$http', function($rootScope, $http) {
-    var service = {
-        groups: {},
+ListApp.controller('ListCtrl', ['$scope', function($scope) {
+    console.log("In ListCtrl");
 
-        addGroup: function(groupName) {},
-        removeGroup: function(groupId) {},
-
-        addTask: function(taskText, taskGroup, taskDueDate, subTasks) {},
-        removeTask: function(taskId) {}
-    };
-
-    $http({
-        method:'GET',
-        url: '/task/get/all'
-    }).success(function(data, status, headers, config) {
-        console.log("TASKS: ", data.data);
-    }).error(function(data, status, headers, config) {
-        console.log("ERROR: Could not retreive tasks.");
-    });
-
-    return service;
-}]);
-
-taskApp.controller('taskCtrl', ['Tasks', function taskCtrl($scope, $http, Tasks) {
-
-    console.log("In taskCtrl");
-    $scope.User = {};
-    // TODO: Discuss data format and presentation
-    $scope.TaskGroups = $http({method: "GET",
-                                url: "/task/get/all"
-                               }).success(function(data, status, headers, config){
-                                   return data.Data;
-                               }).error(function(data, status, headers, config){
-                                   console.log("ERROR: Could not retrieve tasks.");
-                                   // Some kind of flashy div error message. Maybe try again as well?
-                                   return {};
-                               });
-
-    $scope.FocusedGroup = $scope.TaskGroups[0];
-
-    $scope.TestQuery = function() {
-        $http({method: "GET",
-               url: "/task/get/all"
-              }).success(function(data, status, headers, config){
-                  console.log(data);
-              }).error(function(data, status, headers, config){
-                  console.log("ERROR: Could not retrieve tasks.");
-              });
-    };
-
+    $scope.Lists = [
+        {
+            title:"Chores",
+            items:[
+                "Do the laundry", "Clean the catbox"
+            ]
+        },
+        {
+            title:"Life goals",
+            items:[
+                "Kill all the humans", "Eat too much pizza", "become a cat"
+            ]
+        },
+        {
+            title:"Super cool things to share",
+            items:[
+                "http://golang.org/pkg/database/sql/#DB.Prepare",
+                "http://golang.org/pkg/crypto/",
+                "http://golang.org/pkg/fmt/",
+                "http://golang.org/pkg/fsnotify/",
+                "http://golang.org/pkg/inotify/"
+            ]
+        },
+        {
+            title:"Groceries",
+            items:[
+                "Bananas", "soap", "chips", "milk", "Pasta", "More kitty food"
+            ]
+        }
+    ];
 
 }]);
